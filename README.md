50b68de581cc478cc3bef8a1fbe36379d4896383
Jorge Silva <jorge@fcn.edu.br>
2024-11-15 23:46:53 -0300

    Implementação basica bot

---
+++ README.md
@@ -0,0 +1,18 @@
+
+Bot para Discord escrito em Python
+
+Preparando embiente em Windows 11
+
+```shell
+python -m venv .venv
+source .venv/Scripts/activate
+
+python -m pip install --upgrade pip
+pip install poetry
+```
+
+```shell
+poetry init -n
+poetry source show
+```
+
---
+++ src/main.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+
+class Client(discord.Client):
+    async def on_ready(self):
+        print(f'Logado como {self.user}!')
+
+    async def on_message(self, message):
+        #print(f'Mensagem de {message.author}: {message.content}')
+        if message.author == self.user:
+            return
+        
+        if message.content.startswith('ola'):
+            await message.channel.send(f'Oi {message.author}')
+
+    # reagio a uma mensagem com um emojin
+    async def on_reaction_add(self, reaction, use):
+        await reaction.message.channel.send('Você reagio !!!, :heart_eyes: ameri ...')
+
+
+
+
+# =================================
+#
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = Client(intents=intents)
+client.run('')
+
